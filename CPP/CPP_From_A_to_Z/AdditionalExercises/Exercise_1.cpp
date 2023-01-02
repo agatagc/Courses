95bb28a3395e17ff6aa091bab3793241c601835e
Agata Siudem <agatagc98@gmail.com>
2023-01-02 12:06:23 +0100

    Adding exercise 1 instructions.

---
+++ CPP/CPP_From_A_to_Z/AdditionalExercises/Exercise_1.cpp
@@ -0,0 +1,10 @@
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Zadanie 1:
+
+// Program z wyborem (menu) 5 funkcji:
+// 1. Funkcja, która poprosi użytkownika o podanie długości listy z przedziału od 10 do 100, utworzy listę liczb losowych typu float/double, obliczy wartość średnią z liczb na liście i wyświetl tę wartość w konsoli.
+// 2. Funkcja, która prosi użytkownika o wprowadzenie ciągu znaków o minimalnej długości 5 znaków, wyświetli to, co użytkownik wprowadził wielkimi i małymi literami oraz bez znaków „a” i „A”.
+// 3. Funkcja, która wygeneruje listę 10 pozycji, wypełni ją losowymi liczbami typu float/double, posortuje listę od największej do najmniejszej i wyświetli ją.
+// 4. Funkcja, która poprosi użytkownika o wprowadzenie ciągu o minimalnej długości 10 i wyświetli ciąg bez znaków „a”, „e” i „ ” (spacja) oraz wyświetli długość string przed i po usunięciu tych znaków.
+// 5. Gra: Kamień, papier, nożyce. Może to być podane przez użytkownika jako k, p, n lub 1, 2, 3. Gra ma być pętlą, która zakończy się, gdy użytkownik wygra. Gra ma na celu losowe wybranie własnego wyboru (kamienia, papieru lub nożyczek) i poproszenie gracza o dokonanie własnego wyboru.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
