5b6aeaa9346228cb9b350661da6de4796549d8c2
Agata Siudem <agatagc98@gmail.com>
2023-02-28 07:42:56 +0100

    Updating section 11
    Function parameters

--- CPP/CPP_From_Beginner_to_Beyond/Section_11/Function_Parmameters.cpp
+++ CPP/CPP_From_Beginner_to_Beyond/Section_11/Function_Parmameters.cpp
@@ -20,9 +20,45 @@ void pass_by_value2(string s)
     s = "Changed";
 }
 
+void pass_by_value3(vector<string> v)
+{
+    v.clear();      // delete all vector elements
+}
+
 
 void pass_by_value3(vector<string> v) 
 {
     for (auto s: v)
-        cout << "";
+        cout << s << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int num {10};
+    int another_num {20};
+
+    cout << "num before calling pass_by_value1: " << num << endl;
+    pass_by_value1(num);
+    cout << "num after caling pass_by_value1:" << num << endl;
+
+    cout << "\nanother_num before calling pass_by_value1: " << another_num << endl;
+    pass_by_value1(another_num);
+    cout << "another_num after calling pass_by_value1: " << another_num << endl;
+
+
+    string name {"Agata"};
+    cout << "\nname before calling pass_by_value2" << name << endl;
+    pass_by_value2(name);
+    cout << "name after calling pass_by_value2: " << name << endl;
+
+    vector<string> stooges {"Lary", "Moe", "Curly"};
+    cout << "\nstooges before calling pass_by_value3";
+    print_vector(stooges);
+    pass_by_value3(stooges);
+    cout << "stooges before calling pass_by_value3: ";
+    print_vector(stooges);
+
+    cout << endl;
+    return 0;   
 }
