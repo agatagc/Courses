53d246154730427d3eb35208eb975a69768dfccf
Agata Siudem <118853752+agatagc@users.noreply.github.com>
2023-09-17 21:20:10 +0200

    Adding challange 1 solution with 2 sorting algorithms

---
+++ CSharp/Challange1_SortingListOfNumbers/Challange1_SortingListOfNumbers/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Challange1_SortingListOfNumbers
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        List<int> numbers = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void UpdateListbox(List<int> numbers)
+        {
+            listBox1.Items.Clear(); // Deleting all numbers in listbox
+
+            foreach (int number in numbers)
+            {
+                listBox1.Items.Add(number); // Adding sorted numbers to listbox
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numbers.Clear(); // Deleting all numbers int list
+
+            for (int i = 0; i <= 100; i++)
+            {
+                numbers.Add(rand.Next(1, 101)); // Creating number
+            }
+
+            UpdateListbox(numbers);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> numbers = new List<int>(this.numbers);
+            int listSize = numbers.Count; // Getting list size
+
+            int iterationCounter = 0; // Creating iteration counter
+
+            for (int i = 0; i < listSize; i++) // Looping through all numbers in list
+            {
+                for (int j = 0; j < listSize; j++)
+                {
+                    if (numbers[i] < numbers[j]) // If number is smaller than number in list
+                    {
+                        int temporaryVariable = numbers[i]; // Creating temp variable
+                        numbers[i] = numbers[j]; // Setting number to number in list
+                        numbers[j] = temporaryVariable; // Setting number in list to temp variable
+                    }
+
+                    iterationCounter++; // Adding 1 to iteration counter
+                }
+            }
+
+            label2.Text = $"Bubble sort iteration count: {iterationCounter}";
+
+            UpdateListbox(numbers); // Updating listbox
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<int> numbers = new List<int>(this.numbers); // Creating a copy of numbers list
+            int listSize = numbers.Count; // Getting list size
+            int iterationCounter = 0; // Creating iteration counter
+
+            for (int i = 0; i < listSize; i++) // Looping through all numbers in list
+            {
+                for (int j = i + 1; j < listSize; j++)
+                {
+                    iterationCounter += 1;
+
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temporaryVariable = numbers[i]; // Creating temp variable
+                        numbers[i] = numbers[j]; // Setting number to number in list
+                        numbers[j] = temporaryVariable; // Setting number in list to temp variable
+                    }
+                }
+            }
+
+            label2.Text = $"Custom sort 1 iteration count: {iterationCounter}";
+            UpdateListbox(numbers); // Updating listbox
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<int> numbers = new List<int>(this.numbers); // Creating a copy of numbers list
+            int listSize = numbers.Count; // Getting list size
+            int iterationCounter = 0; // Creating iteration counter
+
+
+
+            label2.Text = $"Custom sort 2 iteration count: {iterationCounter}";
+            UpdateListbox(numbers); // Updating listbox
+        }
+    }
+}
---
+++ CSharp/PetlaForSredniaOcen/PetlaForSredniaOcen/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetlaForSredniaOcen
+{
+    public partial class Form1 : Form
+    {
+        double average;
+        public Form1()
+        {
+            InitializeComponent();
+            Table();
+        }
+
+        void Table()
+        {
+            double sumOfGrades = 0; // suma ocen, zaczynamy od 0, poniewaz od tej wartosci zaczynamy sumowanie
+            double [] grades = new double[]
+            {
+                4, 5, 6, 2, 3, 5, 4, 4, 4, 1, 4, 4, 4, 5, 5, 1, 1, 1
+            };
+
+            for (int i = 0; i < grades.Length; i++) // length - dlugosc tablicy
+            {
+                sumOfGrades += grades[i]; 
+
+                average = sumOfGrades / grades.Length;
+                label1.Text = "Your grade point average is " + average;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
