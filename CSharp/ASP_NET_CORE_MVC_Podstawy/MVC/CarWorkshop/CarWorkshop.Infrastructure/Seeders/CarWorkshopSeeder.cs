a81c2b9edf66e1018d308f03c833be30dad372df
Agata Siudem <agatagc98@gmail.com>
2024-02-23 16:30:08 +0100

    Uploading MVC Project

---
+++ CSharp/ASP_NET_CORE_MVC_Podstawy/MVC/CarWorkshop/CarWorkshop.Infrastructure/Seeders/CarWorkshopSeeder.cs
@@ -0,0 +1,44 @@
+﻿using CarWorkshop.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infrastructure.Seeders
+{
+    public class CarWorkshopSeeder
+    {
+        private readonly CarWorkshopDbContext dbContext;
+
+        public CarWorkshopSeeder(CarWorkshopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task Seed() 
+        { 
+            if (await dbContext.Database.CanConnectAsync())
+            {
+                if (!dbContext.CarWorkshops.Any())
+                {
+                    var mazdaAso = new Domain.Entities.CarWorkshop()
+                    {
+                        Name = "Mazda ASO",
+                        Description = "Mazda authorized service",
+                        ContactDetailes = new()
+                        {
+                            City = "Warsaw",
+                            Street = "Marszałkowska",
+                            PostalCode = "20-060",
+                            PhoneNumber = "+48-123-456-789",
+                        }
+                    };
+                    mazdaAso.EncodeName();
+                    dbContext.CarWorkshops.Add(mazdaAso);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/ASP_NET_CORE_MVC_Podstawy/MVC/CarWorkshop/CarWorkshop.MVC/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWorkshop.MVC.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = "FirstName";
+        public string LastName { get; set; } = "LastName";
+    }
+}
